2e02f0c0c0b6616e4fa3ab4ea47a7f334f4c31dc
zRedk <federica.mosca2001@gmail.com>
2025-02-17 17:59:46 +0100

    edited text in endview

--- Pyris.swiftpm/View/EndGameView.swift
+++ Pyris.swiftpm/View/EndGameView.swift
@@ -21,9 +21,12 @@ struct EndGameView: View {
                            height: geometry.size.height)
                     .opacity(0.7)
                 
-                VStack {
-                    Text("You blowed all the wind out!")
-                        .font(.system(size: 48))
+                VStack (alignment: .center) {
+                    
+                    Spacer()
+                    
+                    Text("Congratulations!")
+                        .font(.system(size: 48, weight: .bold))
                         .fontWidth(.expanded)
                         .multilineTextAlignment(.center)
                         .foregroundStyle(.white)
@@ -31,6 +34,35 @@ struct EndGameView: View {
                         .padding()
                         .background(Color.black.opacity(0.7))
                         .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    Spacer()
+                        .frame(maxHeight:100)
+                    
+                    Text("You’ve helped Pyris find calm and clear the storm!")
+                        .font(.system(size: 32))
+                        .fontWidth(.expanded)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.white)
+                        .shadow(color: .accentColor, radius: 10)
+                        .padding()
+                        .background(Color.black.opacity(0.5))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding(.horizontal, 64)
+                    
+                    Spacer()
+                    
+                    Image("Pyris")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, alignment: .bottom)
+                        .shadow(color: .accentColor, radius: 10)
+                        .scaleEffect(1, anchor: .bottom)
+                        //.transition(.scale.animation(.easeInOut(duration: 2)))
+                        //.animation(animation, value: 1)
+                    
+                    Spacer()
+                        .frame(height: 50)
+                        .padding(.vertical, 32)
                 }
             }
             .transition(.opacity.animation(.easeInOut(duration: 1)))
